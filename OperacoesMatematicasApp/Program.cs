40cc5808f801f8a47bf0240e33544ed143199537
Filipe Menegusso Ferreira <filipe.ferreira@sistemaprismatec.com.br>
2020-08-10 14:08:01 -0300

    Add project files.

---
+++ OperacoesMatematicasApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OperacoesMatematicasApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine
+        }
+
+        static double LerNumeroValido(string nomeNumero)
+        {
+            Console.WriteLine($"Digite um número ${nomeNumero}: ");
+            var numero = Console.ReadLine();
+
+            if (double.TryParse(numero, out double resultado)) return resultado;
+
+            return 0;
+        }
+    }
+}
